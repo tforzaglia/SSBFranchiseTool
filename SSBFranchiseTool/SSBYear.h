4100d147aef09af7f4f444d647c9967a14a693b4
Tom Forzaglia <thomas.forzaglia@gmail.com>
2014-02-22 23:33:50 -0500

    adding year object class

---
+++ SSBFranchiseTool/SSBYear.h
@@ -0,0 +1,17 @@
+//
+//  SSBYear.h
+//  SSBFranchiseTool
+//
+//  Created by Thomas Forzaglia on 2/22/14.
+//  Copyright (c) 2014 Thomas Forzaglia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSBYear : NSObject
+
+@property (nonatomic, copy) NSArray *matches;
+@property (nonatomic, copy) NSArray *fighterWinners;
+@property (nonatomic, copy) NSArray *ownerWinners;
+
+@end
---
+++ SSBFranchiseTool/SSBYear.m
@@ -0,0 +1,17 @@
+//
+//  SSBYear.m
+//  SSBFranchiseTool
+//
+//  Created by Thomas Forzaglia on 2/22/14.
+//  Copyright (c) 2014 Thomas Forzaglia. All rights reserved.
+//
+
+#import "SSBYear.h"
+
+@implementation SSBYear
+
+@synthesize matches = _matches;
+@synthesize fighterWinners = _fighterWinners;
+@synthesize ownerWinners = _ownerWinners;
+
+@end
